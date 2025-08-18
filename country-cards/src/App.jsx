205aa25e53d2998d87c75feb2dae11bf3c928bc2
Hi-Creatives1911 <ebereihechukwu@gmail.com>
2025-08-18 16:16:29 +0100

    commit 18/08/25

---
+++ country-cards/src/App.jsx
@@ -0,0 +1,69 @@
+import "./App.css";
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [countries, setCountries] = useState([]);
+  const [allCountries, setAllCountries] = useState([]); // Store original list
+
+  const handleSearch = (e) => {
+    const value = e.target.value;
+    setSearchTerm(value);
+    if (value === "") {
+      setCountries(allCountries);
+    } else {
+      const filtered = allCountries.filter((country) =>
+        country.name.toLowerCase().includes(value.toLowerCase())
+      );
+      setCountries(filtered);
+    }
+  };
+
+  useEffect(() => {
+    fetch(
+      "https://raw.githubusercontent.com/stefanbinder/countries-states/master/countries.json"
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setCountries(data);
+        setAllCountries(data);
+      });
+  }, []);
+
+  return (
+    <>
+      <div className="searchContainer">
+        <input
+          type="text"
+          placeholder="Search Countries..."
+          className="searchBar"
+          onChange={handleSearch}
+          value={searchTerm}
+        />
+      </div>
+      <ul className="countries">
+        {countries.map((c) => (
+          <li>
+            <h2 className="countryTitle">{c.name}</h2>
+            <img
+              src={`https://flagcdn.com/${c.code2.toLowerCase()}.svg`}
+              alt={c.name}
+              className="countryFlag"
+            />
+            <div className="countryDetails">
+              <h3>Capital</h3>
+              {c.capital}
+              <br />
+              <h3>Region</h3>
+              {c.region} <br />
+              <h3>SubRegion</h3>
+              {c.subregion} <br />
+            </div>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default App;
